681eb8248addeeb9d43000a8da4dd4cbe88fc5fa
opzsuresh <suresh.y@opz0.io>
2023-08-31 15:57:15 +0530

    Initial commit

---
+++ example/example.tf
@@ -0,0 +1,23 @@
+provider "google" {
+  project = "opz0-397319"
+  region  = "asia-northeast1"
+  zone    = "asia-northeast1-a"
+
+}
+
+
+module "vpc" {
+  source                                    = "./../"
+  name                                      = "vpc"
+  environment                               = "dev"
+  project_id                                = "opz0-397319"
+  routing_mode                              = "REGIONAL"
+  mtu                                       = 1500
+  google_compute_network_enabled            = true
+  module_enabled                            = true
+  delete_default_routes_on_create           = true
+  enable_ula_internal_ipv6                  = false
+  internal_ipv6_range                       = null
+  network_firewall_policy_enforcement_order = "AFTER_CLASSIC_FIREWALL"
+
+}
---
+++ main.tf
@@ -0,0 +1,21 @@
+module "labels" {
+  source      = "git::git@github.com:opz0/terraform-gcp-labels.git?ref=master"
+  name        = var.name
+  environment = var.environment
+  label_order = var.label_order
+
+}
+
+resource "google_compute_network" "vpc" {
+  count                                     = var.google_compute_network_enabled && var.module_enabled ? 1 : 0
+  project                                   = var.project_id
+  name                                      = var.name
+  description                               = var.description
+  routing_mode                              = var.routing_mode
+  mtu                                       = var.mtu
+  enable_ula_internal_ipv6                  = var.enable_ula_internal_ipv6
+  internal_ipv6_range                       = var.internal_ipv6_range
+  delete_default_routes_on_create           = var.delete_default_routes_on_create
+  network_firewall_policy_enforcement_order = var.network_firewall_policy_enforcement_order
+
+}
